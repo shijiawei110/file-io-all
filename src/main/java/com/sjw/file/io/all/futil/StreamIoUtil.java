512b69a5a54849f04155c41129169629114a28e4
shijiawei <81356394>
2019-06-10 11:39:42 +0800

    鸡你太美之sjw天才初始化

--- src/main/java/com/sjw/file/io/all/futil/StreamIoUtil.java
+++ src/main/java/com/sjw/file/io/all/futil/StreamIoUtil.java
@@ -1,9 +1,16 @@
 package com.sjw.file.io.all.futil;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * @author shijiawei
  * @version StreamIoUtil.java -> v 1.0
  * @date 2019/6/5
  */
-public class StreamIoUtil {
+public class StreamIoUtil implements FileStandardUtil{
+    @Override
+    public void sequenceWrite(File file, byte[] bytes) throws IOException {
+
+    }
 }
